07ec5755c1d2a924c442a6a0f6a4742db2e50f6f
ShaikNihal0009 <73374798+ShaikNihal0009@users.noreply.github.com>
2025-03-03 15:58:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleAuth(option) {
+    let slider = document.querySelectorAll('.toggle .slider')[0];
+    let buttons = document.querySelectorAll('.toggle-btn');
+
+    if (option === 'login') {
+        slider.style.left = "0";
+        buttons[0].classList.add("active");
+        buttons[1].classList.remove("active");
+    } else {
+        slider.style.left = "50%";
+        buttons[0].classList.remove("active");
+        buttons[1].classList.add("active");
+    }
+}
+
+function toggleRole(option) {
+    let slider = document.querySelectorAll('.toggle .slider')[1];
+    let buttons = document.querySelectorAll('.toggle-btn');
+
+    if (option === 'seeker') {
+        slider.style.left = "0";
+        buttons[2].classList.add("active");
+        buttons[3].classList.remove("active");
+    } else {
+        slider.style.left = "50%";
+        buttons[2].classList.remove("active");
+        buttons[3].classList.add("active");
+    }
+}
+
+function showSection(sectionId) {
+    document.getElementById("about").classList.add("hidden");
+    document.getElementById("offer").classList.add("hidden");
+    document.getElementById("contact").classList.add("hidden");
+
+    document.getElementById(sectionId).classList.remove("hidden");
+}
